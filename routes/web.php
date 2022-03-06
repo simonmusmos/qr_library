51235d720856d24fc82dde58750d8e146ee8f5a5
Simon Musmos <simonmusmos@gmail.com>
2022-03-07 04:40:18 +0800

    fix redirection

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return route('login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {

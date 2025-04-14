ca841e1344f10fa9c817ffb0eec8b064339bfa84
Simon Musmos <simon.musmos@livline.jp>
2025-04-14 23:40:15 +0800

    fix: typo

--- resources/views/vendor/jetstream/components/welcome-page.blade.php
+++ resources/views/vendor/jetstream/components/welcome-page.blade.php
@@ -5,7 +5,7 @@
                 
                     <img class="logo-jumbo" id="qr_logo_holder" src="{{ url('/images/logo.png') }}" />
 
-                    <p class="logo-jumbo-text">Welccome to Cavite State University - CCAT Campus Library</p>
+                    <p class="logo-jumbo-text">Welcome to Cavite State University - CCAT Campus Library</p>
 
                     <div style="width: 0; overflow:hidden;">
                         <input type="text" id="student-id" class="scanner-input">
